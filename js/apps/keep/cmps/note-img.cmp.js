acc4ce683b6b4a78e8b091cb6c3dcb1b04b53eb9
Tamir Belisha <Tamirbelisha@gmail.com>
2022-03-02 23:11:08 +0200

    rendering works, support pin, mark, clone

--- js/apps/keep/cmps/note-img.cmp.js
+++ js/apps/keep/cmps/note-img.cmp.js
@@ -4,10 +4,10 @@ import noteActions from "./note-actions.cmp.js"
 export default {
   props: ["info", "cmpData"],
   template: `
-        <section class="note-card">
+        <section class="note-card" :style="info.style" :class="markNote">
           <h5>{{info.title}}</h5>
             <img :src="info.url" :title="info.title">
-            <note-actions @delete="deleteNote" @setColor="setColor" :noteType="cmpData.type"></note-actions>
+            <note-actions @delete="deleteNote" @setColor="setColor" @setPin="setPin" @setMark="setMark" @setClone="setClone" :noteType="cmpData.type"></note-actions>
         </section>
     `,
   components: {
@@ -23,12 +23,40 @@ export default {
   },
   methods: {
     deleteNote(){
-      noteService.remove(this.noteData.id)
+      noteService.remove(this.noteData.id).then(()=>{
+        this.$emit('updateData')
+      })
     },
     setColor(color) {
       this.noteData.style.backgroundColor = color
+      noteService.save(this.noteData)
+    },
+    setPin() {
+      if(!this.noteData.isPinned ) {
+        this.noteData.isPinned = true
+      } else this.noteData.isPinned = !this.noteData.isPinned 
+      noteService.save(this.noteData)
+    },
+    setMark() {
+      if(!this.noteData.isMarked){
+        this.noteData.isMarked = true
+      } else this.noteData.isMarked = !this.noteData.isMarked
+      noteService.save(this.noteData).then(()=>{
+        this.$emit('updateData')
+      })
+    },
+    setClone() {
+      let copyNote = {...this.noteData}
+      copyNote.id = null
+      noteService.save(copyNote).then(()=>{
+        this.$emit('updateData')
+      })
     }
   },
-  computed: {},
+  computed: {
+    markNote() {
+      if(this.noteData.isMarked) return 'marked-note'
+    }
+  },
   unmounted() {},
 }
--- js/apps/keep/cmps/note-list.cmp.js
+++ js/apps/keep/cmps/note-list.cmp.js
@@ -1,5 +1,6 @@
 import {noteService} from '../services/note.service.js'
-import notePreview from '../cmps/note-preview.cmp.js'
+import {eventBus} from "../../../services/eventBus-service.js"
+import { storageService } from '../../../services/async-storage-service.js'
 import noteTxt from '../cmps/note-txt.cmp.js'
 import noteImg from '../cmps/note-img.cmp.js'
 import noteTodos from '../cmps/note-todos.cmp.js'
@@ -10,12 +11,17 @@ export default {
   template: `
         <section class="note-cards-container main-layout">
           <div v-for="(cmp, idx) in notes">
-            <component :is="cmp.type"  :info="{...cmp.info}" :cmpData="{...cmp}" :style="cmp.style" @setVal="setAns($event, idx)"></component>
+            <component v-if="cmp.isPinned" :is="cmp.type"  :info="{...cmp.info}" :cmpData="{...cmp}" :style="cmp.style" @updateData="updateData"></component>
+          </div>
+        </section>
+        <hr>
+        <section class="note-cards-container main-layout">
+          <div v-for="(cmp, idx) in notes">
+            <component v-if="!cmp.isPinned" :is="cmp.type"  :info="{...cmp.info}" :cmpData="{...cmp}" :style="cmp.style" @updateData="updateData"></component>
           </div>
         </section>
     `,
   components: {
-    notePreview,
     noteTxt,
     noteImg,
     noteTodos,
@@ -30,12 +36,15 @@ export default {
     noteService.query().then(noteList =>{
       this.notes = noteList
       })
+    this.unsubscribe = eventBus.on('updateByBus', this.updateData)
   },
   methods: {
-    setVal() {
-      console.log('thats a card');
+    updateData() {
+      noteService.query().then(noteList => this.notes = noteList)
     }
   },
   computed: {},
-  unmounted() {},
+  unmounted() {
+    this.unsubscribe()
+  },
 }
