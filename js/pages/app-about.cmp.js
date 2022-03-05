5f2074f273fbb0f53546221ec5bb7e885c72dfe1
Tamir Belisha <Tamirbelisha@gmail.com>
2022-03-05 18:51:51 +0200

    New design!

--- js/pages/app-about.cmp.js
+++ js/pages/app-about.cmp.js
@@ -1,7 +1,26 @@
 export default {
   // props: [""],
   template: `
-        <section>
+        <section class="main-content home-page flex justify-center">
+          <div class="homepage-header">About <span>Appsus</span></div>
+          <div class="about-content">
+            <h3>Our app was created to serve well anyone</h3>
+            <p>Who needs to manage his time, remind himself his tasks, Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Excepturi vero veritatis eos atque, sapiente doloribus officia quia amet eaque accusantium voluptatem fugit libero
+                eligendi expedita perspiciatis deleniti cum laudantium soluta.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eligendi voluptates tempore? Nulla, atque! Atque,
+                 pariatur amet et adipisci temporibus non sapiente dicta, officia commodi, molestias vitae ullam id aliquam.
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, delectus blanditiis aliquid amet 
+                 numquam harum consequuntur voluptates accusantium reprehenderit saepe tenetur ullam in eum ea neque odit quibusdam praesentium? Laborum.
+                 Who needs to manage his time, remind himself his tasks, Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Excepturi vero veritatis eos atque, sapiente doloribus officia quia amet eaque accusantium voluptatem fugit libero
+                eligendi expedita perspiciatis deleniti cum laudantium soluta.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eligendi voluptates tempore? Nulla, atque! Atque,
+                 pariatur amet et adipisci temporibus non sapiente dicta, officia commodi, molestias vitae ullam id aliquam.
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, delectus blanditiis aliquid amet 
+                 numquam harum consequuntur voluptates accusantium reprehenderit saepe tenetur ullam in eum ea neque odit quibusdam praesentium? Laborum.
+              </p>
+          </div>
         </section>
     `,
   components: {},
